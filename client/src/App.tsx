0b02ab90b6237f7e5c92a78604d3a22778998d4c
Justin Shaber <justinshaber@gmail.com>
2025-06-03 23:33:07 -0700

    Implement toggle logic for AddForm visibility

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,64 +1,43 @@
 // import { useState } from 'react'
 import './public/stylesheets/main.css'
 import ProductList from './components/ProductList'
+import AddForm from './components/AddForm'
+import { useState } from 'react';
+
+
 
 function App() {
+  const [showAddButton, setShowAddButton] = useState(true);
+
+  function handleToggleAddButton() {
+    setShowAddButton(!showAddButton);
+  }
+
   return (
     <>
       <div id="app">
-    <header>
-      <h1>The Shop!</h1>
-      <div className="cart">
-        <h2>Your Cart</h2>
-        <p>Your cart is empty</p>
-        <p>Total: $0</p>
-        <button className="checkout" disabled>Checkout</button>
-      </div>
-    </header>
+        <header>
+          <h1>The Shop!</h1>
+          <div className="cart">
+            <h2>Your Cart</h2>
+            <p>Your cart is empty</p>
+            <p>Total: $0</p>
+            <button className="checkout" disabled>Checkout</button>
+          </div>
+        </header>
 
-    <main>
-      <ProductList />
-
-      <div className="add-form">
-        <form>
-          <div className="input-group">
-            <label htmlFor="product-name">Product Name:</label>
-            <input
-              type="text"
-              id="product-name"
-              name="product-name"
-              required
-            />
-          </div>
-          <div className="input-group">
-            <label htmlFor="product-price">Price:</label>
-            <input
-              type="number"
-              id="product-price"
-              name="product-price"
-              min="0"
-              step="0.01"
-              required
-            />
-          </div>
-          <div className="input-group">
-            <label htmlFor="product-quantity">Quantity:</label>
-            <input
-              type="number"
-              id="product-quantity"
-              name="product-quantity"
-              min="0"
-              required
-            />
-          </div>
-          <div className="actions form-actions">
-            <button type="submit">Add</button>
-            <button type="button">Cancel</button>
-          </div>
-        </form>
+        <main>
+          <ProductList />
+          {showAddButton ? 
+            (<p>
+              <button className="add-product-button" onClick={handleToggleAddButton}>
+                Add A Product
+              </button>
+            </p>) : 
+            (<AddForm onToggleAddButton={handleToggleAddButton}/>)
+          }
+        </main>
       </div>
-    </main>
-  </div>
     </>
   )
 }
