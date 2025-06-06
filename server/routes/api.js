69e98c2082fca7edb58c19f6066768d29f9a3952
Justin Shaber <justinshaber@gmail.com>
2025-06-06 12:16:14 -0700

    changed browser tab name and icon

--- server/routes/api.js
+++ server/routes/api.js
@@ -128,7 +128,6 @@ router.put("/products/:id", (req, res) => {
 });
 
 // doing now
-// friday
 router.delete("/products/:id", (req, res, next) => {
   const productId = req.params.id;
   Product.findByIdAndRemove(productId)
